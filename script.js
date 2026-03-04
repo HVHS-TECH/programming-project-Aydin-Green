cdc5db903253023f4600158290518c682e70ddca
Aydin Green <23194ag@hvhs.school.nz>
2026-03-04 15:04:09 +1300

    red dot mk 2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function setup() {
 	console.log("setup: ");
 	cnv=new Canvas(windowWidth, windowHeight)
-var red = new Sprite (0, 0, 50, 50)
-red.color=255, 0, 0
+var red = new Sprite (windowWidth/2, windowHeight/2, 50, 'd')
+red.color='255, 0, 0'
 }
 
 function draw() {
