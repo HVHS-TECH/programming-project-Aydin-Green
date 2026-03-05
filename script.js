3b4b1f5b5291ddaaf21e74e965dfba336e7961bb
Aydin Green <23194ag@hvhs.school.nz>
2026-03-06 09:18:28 +1300

    text test 2

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function setup() {
 	console.log("setup: ");
 	cnv=new Canvas(windowWidth, windowHeight)
+
 var red = new Sprite (windowWidth/2, windowHeight/2, 50, 'd')
 red.color='#ff0000'
 }
-
+	var Start = 0
+	console.log(Start)
 function draw() {
+	background(0, 0, 255)
+
 	if (Start = 0) {
 		text("Get Green to Red", windowWidth, windowHeight)
 	}
-	
-	background(0, 0, 255)
 }
